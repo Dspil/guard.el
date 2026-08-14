16c9d029454c2d4dc5cfdf833a7db6dd8fffe747
Dspil <dennisspiliopoylos@gmail.com>
2026-08-14 20:36:54 +0200

    License

--- guard.el
+++ guard.el
@@ -1,10 +1,26 @@
 ;;; guard.el --- Custom modular init framework -*- lexical-binding: t -*-
 
+;; Copyright (C) 2026 Dionisis Spiliopoulos
+
 ;; Author: Dionisis Spiliopoulos <dennisspiliopoylos@gmail.com>
 ;; URL: https://github.com/Dspil/guard.el
-;; Version: 1.0.0
 ;; Keywords: convenience, initialization, profiles, tools
 ;; Package-Requires: ((emacs "29.1"))
+
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; Guard is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; Guard is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with Guard.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
